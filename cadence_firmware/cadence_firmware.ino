8b1354d4f1de42308dc03fd5c743166d4f25147b
Devon Bray <dev@esologic.com>
2020-08-04 23:42:16 -0400

    Command protocol working

--- cadence_firmware/cadence_firmware.ino
+++ cadence_firmware/cadence_firmware.ino
@@ -1,24 +1,28 @@
 /*
 
-Cadence Driver - Version 3 - 1/8/2019
+Cadence Driver - Version 3 - 1/8/2019 TODO: update!
 
 This version of Cadence Driver uses none of the given pulsesensor code because the performance was not acceptable.
-This version is based on the interrupt approach by Joel Murphy and Yury Gitman.
 
 Pins:
     Solenoid 1 - D2
     Solenoid 2 - D3
+    Status LED - D5
     Heartbeat Sensor 1 - A0
     Heartbeat Sensor 2 - A1
 
 Usage Notes:
-    Edit "High level configuration constants" to tune most of the application.
-    pulse Sensor sample aquisition and processing happens in the background via Timer 1 interrupt. 1mS sample rate.
-    PWM on pins 9 and 10 will not work when using this code!
+    * Edit "High level configuration constants" to tune most of the application.
+    * Pulse Sensor sample aquisition and processing happens in the background via Timer 1 interrupt. 1mS sample rate.
+    * PWM on pins 9 and 10 will not work when using this code!
     
-    The following variables are automatically updated:
-      QS:     boolean that is made true whenever pulse is found and BPM is updated. User must reset.
+    * The following variables are automatically updated by the ISR:
+      pulse_non_resetting:  boolean that is made true whenever pulse is found. User must reset.
+      pulse_resetting:      boolean that is made true whenever pulse is found. ISR sets to false when the beat has completed.
 
+    * When the host PC connects to twitch, the red LED on the PCB will blink 3 times quickly.
+    * If the host PC becomes disconnected from twitch, the red LED on the PCB will blink twice slowly.
+      
 For support:
     dev@esologic.com
 
@@ -30,6 +34,20 @@ For support:
 
 // Amount in milliseconds to hold solenoids on for
 #define SOLENOID_ENABLE_TIME  100
+#define SERIAL_SOLENOID_PIN_INDEX 1 // 0 or 1 - this is the index in the solenoid pins that will be toggled by the serial port
+#define STATUS_LED_BLINK_OFF_TIME 100 // in ms. The amount of time for the status LED to be off when displaying a blink pattern to the user
+
+/*
+  Communication Protocol Def
+ */
+
+#define COMMAND_HEARTBEAT 0x01        // host PC sending a command for us to respond to to say we're still here
+#define COMMAND_SERVICE_STARTED 0x02  // host PC saying the service is started and we should expect to get commands
+#define COMMAND_SERVICE_CRASHED 0x03  // host PC informing us that they have crashed and we will not be getting any more commands
+#define COMMAND_PULSE_LED 0x04        // host PC is telling us to trigger the solenoid pin, and blink the LED
+#define COMMAND_PULSE_NO_LED 0x05     // host PC is telling us to trigger the solenoid pin without blinking the LED
+
+// Every byte should be responsded to with the same byte.
 
 /*
   Program Body
@@ -37,8 +55,8 @@ For support:
 
 #define NUM_PAIRS 2
 
-volatile unsigned long lastBeatTime[NUM_PAIRS] = {0, 0};  // used to find the time between beats
-volatile unsigned long sampleCounter[NUM_PAIRS] = {0, 0}; // used to determine pulse timing
+volatile unsigned long last_beat_time[NUM_PAIRS] = {0, 0};  // used to find the time between beats
+volatile unsigned long sample_counter[NUM_PAIRS] = {0, 0}; // used to determine pulse timing
 
 // these are volatile because they are used inside of the ISR
 volatile boolean pulse_resetting[NUM_PAIRS] = {false, false};     // true when pulse wave is high, false when it's low
@@ -48,10 +66,11 @@ bool solenoid_enabled[NUM_PAIRS] = {false, false};
 unsigned long solenoid_start[NUM_PAIRS] = {0, 0};
 
 // Pins of all inputs and outputs
-#define  PULSE1_PIN  A0
-#define  PULSE2_PIN  A1
-#define  SOLENOID1_PIN  2
-#define  SOLENOID2_PIN  3
+#define  PULSE1_PIN A0
+#define  PULSE2_PIN A1
+#define  SOLENOID1_PIN 2
+#define  SOLENOID2_PIN 3
+#define  STATUS_LED_PIN 5
 
 int pulse_pins[NUM_PAIRS] = {PULSE1_PIN, PULSE2_PIN};
 int solenoid_pins[NUM_PAIRS] = {SOLENOID1_PIN, SOLENOID2_PIN}; 
@@ -66,21 +85,63 @@ void setup() {
   ICR1 = 8000;   // TRIGGER TIMER INTERRUPT EVERY 1mS  
   sei();         // MAKE SURE GLOBAL INTERRUPTS ARE ENABLED
 
-  // Set up solenoid driver pins
   pinMode(SOLENOID1_PIN, OUTPUT);
   pinMode(SOLENOID2_PIN, OUTPUT);
+  pinMode(STATUS_LED_PIN, OUTPUT);
+
+  Serial.begin(115200);
 
 }
 
+void status_led_blink(int num_blinks, int on_time) {
+  // Blink the LED in a given pattern. Indicates to user how things are going.
+  for (int i = 0; i < num_blinks; i++) {
+    digitalWrite(STATUS_LED_PIN, HIGH);
+    delay(on_time);
+    digitalWrite(STATUS_LED_PIN, LOW);
+    delay(STATUS_LED_BLINK_OFF_TIME);
+  }
+}
+
 void loop() {
+
+  bool serial_solenoid_enabled = false;
+
+  if (Serial.available()) {
+    byte command = Serial.read();
+    switch (command) {
+      case COMMAND_HEARTBEAT:
+        break;
+      case COMMAND_SERVICE_STARTED:
+        status_led_blink(3, 300);  // three short blinks when we expect to start processing commands.
+        break;
+      case COMMAND_SERVICE_CRASHED:
+        status_led_blink(2, 1000); // two long blinks if something bad happens on the pi end of things.
+        break;
+      case COMMAND_PULSE_LED:
+        serial_solenoid_enabled = true;
+        digitalWrite(STATUS_LED_PIN, HIGH);
+        break;
+      case COMMAND_PULSE_NO_LED:
+        serial_solenoid_enabled = true;
+        break;
+    }
+    Serial.write(command);  // echo back
+  }
 
   for (int pair_index = 0; pair_index < NUM_PAIRS; pair_index++) {
     
     bool start_solenoid = false;
 
-    if (pulse_non_resetting[pair_index] == true) {
-      pulse_non_resetting[pair_index] = false;
-      start_solenoid = true; 
+    if ((serial_solenoid_enabled) && (pair_index == SERIAL_SOLENOID_PIN_INDEX)) {
+      start_solenoid = true;
+    }
+
+    if (pair_index != SERIAL_SOLENOID_PIN_INDEX) {
+      if (pulse_non_resetting[pair_index] == true) {
+        pulse_non_resetting[pair_index] = false;
+        start_solenoid = true; 
+      }
     }
 
     if (start_solenoid) {
@@ -95,26 +156,30 @@ void loop() {
       if (millis() - solenoid_start[pair_index] > SOLENOID_ENABLE_TIME) {
         digitalWrite(solenoid_pins[pair_index], LOW);
         solenoid_enabled[pair_index] = false;
+        if (pair_index == SERIAL_SOLENOID_PIN_INDEX) {
+          digitalWrite(STATUS_LED_PIN, LOW);
+        }
       }
     }
-
+  
   }
-   
-   delay(20);
+  
+  delay(10);
 
 }
 
-// THIS IS THE TIMER 1 INTERRUPT SERVICE ROUTINE. 
-ISR(TIMER1_OVF_vect) { // triggered every time Timer 1 overflows, every 1mS
+ISR(TIMER1_OVF_vect) {
+  // THIS IS THE TIMER 1 INTERRUPT SERVICE ROUTINE. 
+  // triggered every time Timer 1 overflows, every 1mS
   for (int pair_index = 0; pair_index < NUM_PAIRS; pair_index++) {
 
     int pulse_signal = analogRead(pulse_pins[pair_index]);  // read the pulse Sensor 
-    sampleCounter[pair_index]++;                            // keep track of the time with this variable (ISR triggered every 1mS
+    sample_counter[pair_index]++;                            // keep track of the time with this variable (ISR triggered every 1mS
 
-    int time_delta = sampleCounter[pair_index]-lastBeatTime[pair_index];  // monitor the time since the last beat to avoid noise
+    int time_delta = sample_counter[pair_index]-last_beat_time[pair_index];  // monitor the time since the last beat to avoid noise
     
     if ( (pulse_signal > 520) && (pulse_resetting[pair_index] == false) && (time_delta > 500) ) {  
-      lastBeatTime[pair_index] = sampleCounter[pair_index];   // keep track of time for next pulse
+      last_beat_time[pair_index] = sample_counter[pair_index];   // keep track of time for next pulse
       pulse_non_resetting[pair_index] = true;                 // set Quantified Self flag when beat is found and BPM gets updated, QS FLAG IS NOT CLEARED INSIDE THIS ISR
       pulse_resetting[pair_index] = true;                     // set the pulse flag when we think there is a pulse
     }                       
@@ -122,6 +187,5 @@ ISR(TIMER1_OVF_vect) { // triggered every time Timer 1 overflows, every 1mS
     if (pulse_signal < 500 && pulse_resetting[pair_index] == true) {  // when the values are going down, it's the time between beats
       pulse_resetting[pair_index] = false;                            // reset the pulse flag so we can do it again!
     }
-
   } 
 }
