acdf73253f75f6d333edd3c45a4b1e1cd3ac89e1
Devon Bray <dev@esologic.com>
2020-09-20 21:50:26 -0400

    Tweaked max negative threshold

--- cadence_firmware/cadence_firmware.ino
+++ cadence_firmware/cadence_firmware.ino
@@ -100,7 +100,7 @@ volatile unsigned long last_sample_time[NUM_PAIRS] = {0, 0}; // used to determin
 #define VALUE_INTO_ANALYSIS_EVERY_N_SAMPLES 10
 #define NUM_HISTORIC_ANALYSIS 400
 #define ANALYSIS_MIN_POSITIVE_THRESHOLD 200
-#define ANALYSIS_MAX_NEGATIVE_THRESHOLD 50
+#define ANALYSIS_MAX_NEGATIVE_THRESHOLD 60
 #define MIN_DISTANCE_FROM_NEGATIVE_ANALYSIS 5000  // in ms
 
 volatile int analysis_history[NUM_PAIRS][NUM_HISTORIC_ANALYSIS] = { 0 };
