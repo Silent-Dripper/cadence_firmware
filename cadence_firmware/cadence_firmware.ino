9559b94cc13d7512212f61a3c551cdb5bd57ffe7
Devon Bray <dev@esologic.com>
2021-01-24 15:19:47 -0500

    Submitting a PR before adding tweaks for production drips.

--- cadence_firmware/cadence_firmware.ino
+++ cadence_firmware/cadence_firmware.ino
@@ -37,7 +37,7 @@ For support:
 
 // If set to True, both drippers will drip given the settings of sound_test_loop.
 // This should NEVER be enabled for production.
-#define SOUND_TEST_MODE_ENABLED true
+#define SOUND_TEST_MODE_ENABLED false
 
 // Configure sending mock commands for testing
 // This should NEVER be enabled for production. 
@@ -83,8 +83,9 @@ For support:
   #define ACTUATOR_2_MOTOR_ENABLE_TIME 210
   // These values are the number of steps the stepper motor will rotate when enabled
   // There is no check to make sure that all of the steps actually execute.
-  #define ACTUATOR_1_MOTOR_DRIVE_STRENGTH 45
-  #define ACTUATOR_2_MOTOR_DRIVE_STRENGTH 45  
+  // TODO - these constants are poorly named
+  #define ACTUATOR_1_MOTOR_DRIVE_STRENGTH 890 // TODO - this should read from a pot to trim the value.
+  #define ACTUATOR_2_MOTOR_DRIVE_STRENGTH ACTUATOR_1_MOTOR_DRIVE_STRENGTH  
 #else
   #error "Invalid ACTUATORS_CONTROL_MODE"
 #endif
@@ -128,45 +129,33 @@ For support:
   Adafruit_DCMotor *actuator_2 = AFMS.getMotor(2);
   Adafruit_DCMotor *actuator_controllers[NUM_PAIRS] = { actuator_1, actuator_2 };
 #elif ACTUATORS_CONTROL_MODE == AC_TMC2208
-
   #include <TMCStepper.h>
-
   #define TMC_BAUD_RATE 115200
-  #define TMC_PDN_DISABLE true
-  #define TMC_I_SCALE_ANALOG 0
-  #define TMC_RMS_CURRENT 1000
-  #define TMC_MICROSTEPS 0
+  #define TMC_PDN_DISABLE true   // Use UART 
+  #define TMC_I_SCALE_ANALOG 0  // Adjust current from the register
+  #define TMC_RMS_CURRENT 1000  // Set driver current 1A
+  #define TMC_MICROSTEPS 16
   #define TMC_IRUN 9
   #define TMC_IHOLD 5
-  #define TMC_GSTAT 0b111
-  
+  #define TMC_GSTAT 0b111 
   #define TMC_1_SW_TX 2  // TMC2208 SoftwareSerial transmit pin
   #define TMC_1_SW_RX 3  // TMC2208 SoftwareSerial receive pin
   #define TMC_1_STEP_PIN 4  // Step
   #define TMC_1_DIR_PIN 5  // Direction
   #define TMC_1_EN_PIN 6  // Enable
-
   #define TMC_2_SW_TX 7  // TMC2208 SoftwareSerial transmit pin
   #define TMC_2_SW_RX 8  // TMC2208 SoftwareSerial receive pin
   #define TMC_2_STEP_PIN 9  // Step
   #define TMC_2_DIR_PIN 10  // Direction
   #define TMC_2_EN_PIN 11  // Enable
-  
   #define R_SENSE 0.11f  // SilentStepStick series use 0.11
-
-  TMC2208Stepper tmc_1 = TMC2208Stepper(TMC_1_SW_RX, TMC_1_SW_TX, R_SENSE); // Software serial
-  TMC2208Stepper tmc_2 = TMC2208Stepper(TMC_2_SW_RX, TMC_2_SW_TX, R_SENSE); // Software serial
-  
-  TMC2208Stepper tmc_controllers[NUM_PAIRS] = { tmc_1, tmc_2 };
-  
+  TMC2208Stepper tmc_controllers[NUM_PAIRS] = { TMC2208Stepper(TMC_1_SW_RX, TMC_1_SW_TX, R_SENSE), TMC2208Stepper(TMC_2_SW_RX, TMC_2_SW_TX, R_SENSE) };
   int tmc_enable_pins[NUM_PAIRS] = { TMC_1_EN_PIN, TMC_2_EN_PIN }; 
   int tmc_step_pins[NUM_PAIRS] { TMC_1_STEP_PIN, TMC_2_STEP_PIN };
-
   // These two variables are used to control the motors
   volatile bool tmc_step_pin_value[NUM_PAIRS] = { false, false}; 
   volatile unsigned long tmc_remaining_steps[NUM_PAIRS] = { 0, 0 };
   volatile unsigned long steps_per_drip[NUM_PAIRS] = { ACTUATOR_1_MOTOR_DRIVE_STRENGTH, ACTUATOR_2_MOTOR_DRIVE_STRENGTH };
-
 #else
   #error "Invalid ACTUATORS_CONTROL_MODE"
 #endif
@@ -182,8 +171,6 @@ For support:
 #define COMMAND_SERVICE_CRASHED 0x03  // host PC informing us that they have crashed and we will not be getting any more commands
 #define COMMAND_PULSE_LED 0x04        // host PC is telling us to trigger the actuator pin, and blink the LED
 #define COMMAND_PULSE_NO_LED 0x05     // host PC is telling us to trigger the actuator pin without blinking the LED
-
-// Every byte should be responsded to with the same byte.
 
 /*
   Program Body
@@ -302,7 +289,6 @@ void change_actuator_state(int actuator_index, bool enabled) {
       #elif ACTUATORS_CONTROL_MODE == AC_MOTOR_SHIELD
         actuator_controllers[actuator_index]->run(RELEASE);
       #elif ACTUATORS_CONTROL_MODE == AC_TMC2208
-        // TODO
         digitalWrite(tmc_enable_pins[actuator_index], HIGH);  // disable the driver
       #endif
     }
@@ -402,12 +388,12 @@ void setup() {
     // If the timer's count ever gets to the value set in OCR2A. 
     // It will reset the timer's count after executing the ISR.
     TCCR2A = (1 << WGM21);
-    // Set the TIMER2 prescaler to 1024.
-    TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20);
-    // Set the compare match register for TIMER2 to trigger with a frequency of 600.96hz.
+    // Set the TIMER2 prescaler to 128.
+    TCCR2B = (1 << CS22) | (0 << CS21) | (1 << CS20);
+    // Set the compare match register for TIMER2 to trigger with a frequency of ~9615.4hz.
     // A rising edge will be sent to the step pin of a TMC every other clock cycle, or in this case every 3mS.
     // If that TMC is enabled.
-    OCR2A = 25;  // = (16*10^6) / (600.96 * 1024) - 1 (must be <255 because it's only 1 byte)
+    OCR2A = 12;  // = (16*10^6) / (3000 * 128) - 1 (must be <255 because it's only 1 byte)
     // Enable the function inside of ISR(TIMER2_COMPA_vect).
     TIMSK2 |= (1 << OCIE2A); 
   #endif
@@ -433,60 +419,48 @@ void setup() {
     pinMode(TMC_2_DIR_PIN, OUTPUT);
     digitalWrite(TMC_2_EN_PIN, LOW);  // enable the driver
 
-    tmc_1.beginSerial(TMC_BAUD_RATE);
-    tmc_2.beginSerial(TMC_BAUD_RATE);
-  
-    tmc_1.begin();  // Initiate pins and registeries
-    tmc_2.begin();  // Initiate pins and registeries
-  
-    tmc_1.pdn_disable(TMC_PDN_DISABLE); // Use UART
-    tmc_1.I_scale_analog(TMC_I_SCALE_ANALOG); // Adjust current from the register
-    tmc_1.rms_current(TMC_RMS_CURRENT); // Set driver current 1A
-  
-    tmc_2.pdn_disable(TMC_PDN_DISABLE); // Use UART
-    tmc_2.I_scale_analog(TMC_I_SCALE_ANALOG); // Adjust current from the register
-    tmc_2.rms_current(TMC_RMS_CURRENT); // Set driver current 1A
-  
-    /*
-    * %0001 … %1000:
-    * 128, 64, 32, 16, 8, 4, 2, FULLSTEP
-    * Reduced microstep resolution.
-    * The resolution gives the number of microstep entries per
-    * sine quarter wave.
-    * When choosing a lower microstep resolution, the driver
-    * automatically uses microstep positions which result in a
-    * symmetrical wave.
-    * Number of microsteps per step pulse = 2^MRES
-    * (Selection by pins unless disabled by GCONF.mstep_reg_select)
-    */
-    tmc_1.microsteps(TMC_MICROSTEPS);
-    tmc_2.microsteps(TMC_MICROSTEPS);
-  
-    /*
-    * IRUN (Reset default=31)
-    * Motor run current (0=1/32 … 31=32/32)
-    * Hint: Choose sense resistors in a way, that normal
-    * IRUN is 16 to 31 for best microstep performance.
-    */
-  
-    // 26 worked for no water
-    tmc_1.irun(TMC_IRUN);
-    tmc_2.irun(TMC_IRUN);
-  
-    /*
-    * IHOLD (Reset default: OTP)
-    * Standstill current (0=1/32 … 31=32/32)
-    * In combination with StealthChop mode, setting
-    * IHOLD=0 allows to choose freewheeling or coil
-    * short circuit (passive braking) for motor stand still.
-    */
-    // 9 worked for no water
-    tmc_1.ihold(TMC_IHOLD);
-    tmc_2.ihold(TMC_IHOLD);
+    for (int i = 0; i < NUM_PAIRS; i++) {
+      
+      tmc_controllers[i].beginSerial(TMC_BAUD_RATE);
+      tmc_controllers[i].begin();  // Initiate pins and registeries
+      tmc_controllers[i].pdn_disable(TMC_PDN_DISABLE);
+      tmc_controllers[i].I_scale_analog(TMC_I_SCALE_ANALOG);
+      tmc_controllers[i].rms_current(TMC_RMS_CURRENT);
+
+      /*
+      * %0001 … %1000:
+      * 128, 64, 32, 16, 8, 4, 2, FULLSTEP
+      * Reduced microstep resolution.
+      * The resolution gives the number of microstep entries per
+      * sine quarter wave.
+      * When choosing a lower microstep resolution, the driver
+      * automatically uses microstep positions which result in a
+      * symmetrical wave.
+      * Number of microsteps per step pulse = 2^MRES
+      * (Selection by pins unless disabled by GCONF.mstep_reg_select)
+      */
+      tmc_controllers[i].microsteps(TMC_MICROSTEPS);
+
+      /*
+      * IRUN (Reset default=31)
+      * Motor run current (0=1/32 … 31=32/32)
+      * Hint: Choose sense resistors in a way, that normal
+      * IRUN is 16 to 31 for best microstep performance.
+      */
+      tmc_controllers[i].irun(TMC_IRUN);
+
+       /*
+      * IHOLD (Reset default: OTP)
+      * Standstill current (0=1/32 … 31=32/32)
+      * In combination with StealthChop mode, setting
+      * IHOLD=0 allows to choose freewheeling or coil
+      * short circuit (passive braking) for motor stand still.
+      */
+      tmc_controllers[i].ihold(TMC_IHOLD); 
+    }
   #endif
 
   pinMode(STATUS_LED_PIN, OUTPUT);
-  pinMode(13, OUTPUT);
 
   for (int pair_index = 0; pair_index < NUM_PAIRS; pair_index++) {
     analysis_history_index[pair_index] = wrapCounter(NUM_HISTORIC_ANALYSIS);
@@ -519,8 +493,10 @@ void loop() {
   
   #if SOUND_TEST_MODE_ENABLED == true
     while (true) {
-      tmc_remaining_steps[0] = 45;
-      delay(500);
+      change_actuator_state(0, true);
+      delay(250);
+      change_actuator_state(0, false);
+      delay(250);
     }
   #endif
 
@@ -607,7 +583,6 @@ void loop() {
 }
 
 // THIS IS THE TIMER1 INTERRUPT SERVICE ROUTINE. 
-// triggered every time Timer 1 overflows, every 1mS
 ISR(TIMER1_COMPA_vect) {
   for (int pair_index = 0; pair_index < NUM_PAIRS; pair_index++) {
     int pulse_signal = analogRead(pulse_sensor_pins[pair_index]);
